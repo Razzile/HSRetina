ceede3d7215159cf48565aaa7dc3af4a6b208ad8
Razzile <comfycallum@gmail.com>
2015-08-03 20:25:09 +0100

    Initial Commit

---
+++ HSRetina/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HSRetina
+//
+//  Created by callum taylor on 03/08/2015.
+//  Copyright (c) 2015 satori. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
